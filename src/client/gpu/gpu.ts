335e0d99f42b16b1ca5be62bf34bf8669ff8e29c
André Furchner <storkner@gmail.com>
2025-05-13 22:09:13 +0200

    buffer creation

--- src/client/gpu/gpu.ts
+++ src/client/gpu/gpu.ts
@@ -3,18 +3,22 @@ import {inject} from "../../shared/injector";
 import {Line} from "../../shared/node/2D/line";
 import {Node} from "../../shared/node/node";
 import {LinePass} from "./line-pass";
+import {Camera} from "../../shared/node/2D/camera";
+
+export let device: GPUDevice
 
 export class GPU {
 
 	eventSystem = inject(EventSystem);
-	lines = new LinePass();
-	device: GPUDevice;
+	lines: LinePass = new LinePass();
 	canvas: HTMLCanvasElement;
+	camera: Camera;
 
 	constructor() {
 		this.eventSystem.listen(Topic.NodeCreate, (node: Node) => {
 			switch (node.constructor) {
 				case Line: this.lines.add(node as Line); return;
+				case Camera: this.camera = node as Camera;
 			}
 		});
 
@@ -32,17 +36,28 @@ export class GPU {
 		try {
 			if (navigator.gpu) {
 				const adapter = await navigator.gpu.requestAdapter({powerPreference: 'high-performance'});
-				this.device = await adapter.requestDevice();
+				device = await adapter.requestDevice();
 			}
 		} finally {
-			document.body.textContent = this.device ? 'benchmark started' : 'No GPU available 😔';
+			if (!device) {
+				document.body.textContent = 'No GPU available 😔';
+			}
 		}
+
+		const context = this.canvas.getContext('webgpu');
+		const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+		context.configure({
+			device,
+			format: presentationFormat,
+			alphaMode: 'opaque'
+		});
+
 	}
 
 	update() {
 		// update buffers
 
-		this.lines.update();
+		this.lines.update(this.camera.transform);
 
 
 		// draw calls
--- src/client/gpu/line-pass.ts
+++ src/client/gpu/line-pass.ts
@@ -1,18 +1,22 @@
 import {Line} from "../../shared/node/2D/line";
+import {device} from "./gpu";
+import {Mat3} from "wgpu-matrix";
 
 export class LinePass {
 
-	lines: Line[] = [];
-	linesNeedUpdate: boolean = true;
+	lines: Line[] = []
+	linesNeedUpdate: boolean = true
 
+	vertexBuffer: GPUBuffer
+	matrixBuffer: GPUBuffer
 
+	constructor() {
 
-	update() {
+	}
+
+	update(camera: Mat3) {
 		if (this.linesNeedUpdate) {
-			// update Line buffers
-
-
-
+			this.updateBuffers();
 			this.linesNeedUpdate = false;
 		}
 
@@ -20,6 +24,53 @@ export class LinePass {
 		// render
 	}
 
+	updateBuffers() {
+		let length = 0;
+		for (const line of this.lines) {
+			length += line.vertices.length;
+		}
+
+		const vertices = new Float32Array(length)
+		const matrices = new Float32Array(9 * this.lines.length);
+		let vOffset = 0;
+		let mOffset = 0;
+		for (const line of this.lines) {
+			vertices.set(line.vertices, vOffset);
+			matrices.set(line.transform, mOffset);
+			vOffset += line.vertices.length;
+			mOffset += line.transform.length;
+		}
+
+		// vertex buffer
+		if (!this.vertexBuffer || this.vertexBuffer.size !== vertices.byteLength) {
+			if (this.vertexBuffer) {
+				this.vertexBuffer.destroy();
+			}
+
+			this.vertexBuffer = device.createBuffer({
+				label: 'Vertex Buffer for Line Strip',
+				size: vertices.byteLength,
+				usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+			});
+		}
+
+		device.queue.writeBuffer(this.vertexBuffer, 0, vertices)
+
+		// matrix buffer
+		if (!this.matrixBuffer || this.matrixBuffer.size !== matrices.byteLength) {
+			if (this.matrixBuffer) {
+				this.matrixBuffer.destroy();
+			}
+
+			this.matrixBuffer = device.createBuffer({
+				label: 'Matrix Buffer for Line Strip',
+				size: matrices.byteLength,
+				usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+			});
+		}
+
+		device.queue.writeBuffer(this.matrixBuffer, 0, matrices);
+	}
 
 	add(node: Line) {
 		this.lines.push(node);
