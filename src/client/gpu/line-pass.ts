9496eb001b66f1784bc30f86dab97dabe3633611
André Furchner <storkner@gmail.com>
2025-05-14 22:58:09 +0200

    indirect buffer

--- src/client/gpu/line-pass.ts
+++ src/client/gpu/line-pass.ts
@@ -9,6 +9,7 @@ export class LinePass {
 
 	vertexBuffer: GPUBuffer
 	matrixBuffer: GPUBuffer
+	indirectBuffer: GPUBuffer
 
 	constructor() {
 
@@ -32,13 +33,29 @@ export class LinePass {
 
 		const vertices = new Float32Array(length)
 		const matrices = new Float32Array(9 * this.lines.length);
+		const indirect = new Uint32Array(this.lines.length * 4);
 		let vOffset = 0;
 		let mOffset = 0;
+		let i = 0;
 		for (const line of this.lines) {
 			vertices.set(line.vertices, vOffset);
 			matrices.set(line.transform, mOffset);
 			vOffset += line.vertices.length;
 			mOffset += line.transform.length;
+
+			// vertexCount
+			indirect[i * 4] = line.vertices.length / 2;
+
+			// instanceCount (each draw is one object)
+			indirect[i * 4 + 1] = 1;
+
+			// firstVertex (all objects use the same vertex data from the start)
+			indirect[i * 4 + 2] = 0;
+
+			// firstInstance (used as object_id to index matrixBuffer)
+			indirect[i * 4 + 3] = i;
+
+			i++;
 		}
 
 		// vertex buffer
@@ -68,8 +85,21 @@ export class LinePass {
 				usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
 			});
 		}
-
 		device.queue.writeBuffer(this.matrixBuffer, 0, matrices);
+
+		// indirect buffer
+		if (!this.indirectBuffer || this.indirectBuffer.size !== indirect.byteLength) {
+			if (this.indirectBuffer) {
+				this.indirectBuffer.destroy();
+			}
+			this.indirectBuffer = device.createBuffer({
+				label: 'Indirect Draw Commands Buffer',
+				size: indirect.byteLength,
+				usage: GPUBufferUsage.INDIRECT | GPUBufferUsage.COPY_DST
+			});
+		}
+		device.queue.writeBuffer(this.indirectBuffer, 0, indirect);
+
 	}
 
 	add(node: Line) {
